0e8f9a1d3c9c22a0fe56577a5925e7871153b2ac
daveman235 <awad.david@gmail.com>
2020-06-18 23:52:58 -0400

    added image to homepage

--- server/client/src/pages/Home.js
+++ server/client/src/pages/Home.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Navbar from '../components/NavBar'
-import { Typography } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
+import NavBar from '../components/NavBar'
+
+import computer from "../images/computer.png"
 
 const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1
+    },
+    imageContainer: {
+        marginLeft: "15em",
+        marginTop: "10em",
+        width: "250px",
+        height: "100px",
+        [theme.breakpoints.only("xs")]: {
+            marginLeft: "3.5em"
+        }
+    },
+    image: {
+        width: "100%",
+        height: "auto"
+    },
     title: {
         textAlign: "center",
         margin: "0",
@@ -21,12 +40,20 @@ export default function Home() {
     const classes = useStyles();
 
     return (
-
-        <div>
-            <Navbar />
-
-        </div>
-
+        <>
+            <NavBar />
+            <div className="fade-in">
+                <div className={classes.root}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} sm={6}>
+                            <div className={classes.imageContainer}>
+                                <img className={classes.image} src={computer} alt="computer" />
+                            </div>
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
+        </>
     )
 }
 
