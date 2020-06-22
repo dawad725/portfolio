0b7e4c4bead1c687719f60dc804535d54583665a
daveman235 <awad.david@gmail.com>
2020-06-21 21:18:44 -0400

    still having issues with form

--- server/client/src/pages/Contact.js
+++ server/client/src/pages/Contact.js
@@ -18,8 +18,8 @@ const useStyles = makeStyles(theme => ({
     },
     formContainer: {
         textAlign: "center",
-        backgroundColor: "blue",
-        padding: "4em",
+        backgroundColor: "#cc4b37",
+        padding: "3em",
         [theme.breakpoints.only("xs")]: {
             padding: "1em",
 
@@ -54,10 +54,21 @@ const useStyles = makeStyles(theme => ({
 
 export default function Contact() {
 
+
     const classes = useStyles();
 
+    const initialState = {
+        name: "",
+        email: "",
+        message: "",
+    }
+
+    const clearState = () => {
+        setValues({ ...initialState });
+    }
 
     const handleInputChange = e => {
+        e.preventDefault();
         const { name, value } = e.target
         setValues({ ...values, [name]: value })
     }
@@ -67,23 +78,39 @@ export default function Contact() {
 
         if (!name || !email || !message) return
 
-        sendEmail(values);
 
+
+        console.log("values", values)
+        sendEmail(values);
+        clearState();
     }
 
-    const [values, setValues] = useState({ name: '', email: '', message: '' })
+
+    const [values, setValues] = useState(initialState)
+    // console.log("values", values)
 
     function sendEmail(values) {
         const service_id = "default_service";
         const template_id = "portfoliosite";
         const user_id = 'user_CpSZxOogktSxfnutjVVbH'
 
-        emailjs.sendForm(service_id, template_id, values, user_id)
+        console.log("VAL", values)
+
+        const template_params = {
+            name: values.name,
+            email: values.email,
+            message: values.message
+        }
+
+        // console.log("checking ", template_params)
+        emailjs.sendForm(service_id, template_id, template_params, user_id)
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
                 console.log(error.text);
             });
+
+
     }
 
 
@@ -96,74 +123,62 @@ export default function Contact() {
                 <div className={classes.pageBanner}>
                     <Typography variant="h3">
                         Contact
-                            </Typography>
+                    </Typography>
                 </div>
 
                 <div className={classes.container}>
-                    <Grid spacing={1} container className={classes.formContainer}>
-                        <Grid item xs={12} sm={6}>
-                            <label className={classes.labels}>Name</label>
-                            <TextField
-                                name="name_value"
-                                fullWidth={true}
-                                value={values.name}
-                                variant="outlined"
-                                onChange={handleInputChange}
-                                className={classes.textField}
-                            />
-                        </Grid>
+                    <form onSubmit={addItem}>
+                        <Grid spacing={1} container className={classes.formContainer}>
+                            <Grid item xs={12} sm={6}>
+                                <label className={classes.labels}>Name</label>
+                                <TextField
+                                    name="name"
+                                    fullWidth={true}
+                                    value={values.name}
+                                    variant="outlined"
+                                    onChange={handleInputChange}
+                                    className={classes.textField}
+                                />
+                            </Grid>
 
-                        <Grid item xs={12} sm={6} >
-                            <label className={classes.labels}>Email</label>
-                            <TextField
-                                name="email_value"
-                                fullWidth={true}
-                                value={values.email}
-                                variant="outlined"
-                                onChange={handleInputChange}
-                                className={classes.textField}
-                            />
-                        </Grid>
-                        <Grid item xs={12} >
-                            <label className={classes.labels}>Message</label>
-                            <TextField
-                                id="outlined-multiline-static"
-                                name="message_value"
-                                multiline
-                                fullWidth={true}
-                                rows={4}
-                                value={values.message}
-                                variant="outlined"
-                                onChange={handleInputChange}
-                                className={classes.textField}
-                            />
-                        </Grid>
-                        <Grid item xs={12} className={classes.button}>
-                            <Button
-                                className={classes.button}
-                                color="primary"
-                                variant="contained"
-                                onClick={addItem}
-                            >
-                                Submit
+                            <Grid item xs={12} sm={6} >
+                                <label className={classes.labels}>Email</label>
+                                <TextField
+                                    name="email"
+                                    fullWidth={true}
+                                    value={values.email}
+                                    variant="outlined"
+                                    onChange={handleInputChange}
+                                    className={classes.textField}
+                                />
+                            </Grid>
+                            <Grid item xs={12} >
+                                <label className={classes.labels}>Message</label>
+                                <TextField
+                                    id="outlined-multiline-static"
+                                    name="message"
+                                    multiline
+                                    fullWidth={true}
+                                    rows={4}
+                                    value={values.message}
+                                    variant="outlined"
+                                    onChange={handleInputChange}
+                                    className={classes.textField}
+                                />
+                            </Grid>
+                            <Grid item xs={12} className={classes.button}>
+                                <Button
+                                    className={classes.button}
+                                    color="primary"
+                                    variant="contained"
+                                    onClick={addItem}
+                                >
+                                    Submit
                             </Button>
 
+                            </Grid>
                         </Grid>
-
-
-                        {/* <form className="contact-form" onSubmit={sendEmail}>
-                                <input type="hidden" name="contact_number" />
-                            <label>Name</label>
-                            <input type="text" name="user_name" />
-                            <label>Email</label>
-                            <input type="email" name="user_email" />
-                            <label>Message</label>
-                            <textarea name="message" />
-                            <input type="submit" value="Send" />
-                            </form> */}
-
-
-                    </Grid>
+                    </form>
                 </div>
 
 
