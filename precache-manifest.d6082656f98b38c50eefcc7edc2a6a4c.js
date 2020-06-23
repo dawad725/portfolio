203ebf341d6795b09a70b4fa719b338da0f511c7
daveman235 <awad.david@gmail.com>
2020-06-23 13:06:39 -0400

    Updates

---
+++ precache-manifest.d6082656f98b38c50eefcc7edc2a6a4c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a870933689dd136596fdc2f541d2cdb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5e70ce546aa726abb2b4",
+    "url": "/portfolio/static/css/main.3c48a347.chunk.css"
+  },
+  {
+    "revision": "c7554cff89a1fdcc933e",
+    "url": "/portfolio/static/js/2.0236c30e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolio/static/js/2.0236c30e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e70ce546aa726abb2b4",
+    "url": "/portfolio/static/js/main.99a4b64e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "0852556e3166f7030a7aab61247cee5c",
+    "url": "/portfolio/static/media/Medline.0852556e.png"
+  },
+  {
+    "revision": "8d0bdf268b7a4b796350d4b1f4e70062",
+    "url": "/portfolio/static/media/Portfolio.8d0bdf26.png"
+  },
+  {
+    "revision": "bc4adf7c7a5156fa61c62fc7bdfc5b68",
+    "url": "/portfolio/static/media/WeatherApp.bc4adf7c.png"
+  },
+  {
+    "revision": "1ec52ad53b0be8962ec0425130f7863c",
+    "url": "/portfolio/static/media/computer.1ec52ad5.png"
+  },
+  {
+    "revision": "c8827f8fd842c0bb1eecc5625fbf83f0",
+    "url": "/portfolio/static/media/dave.c8827f8f.png"
+  },
+  {
+    "revision": "30bec5645d47c353314415f7ae845de1",
+    "url": "/portfolio/static/media/illuminate.30bec564.png"
+  },
+  {
+    "revision": "af89e21d8546af8390a9b69a08f9f9bd",
+    "url": "/portfolio/static/media/resume.af89e21d.pdf"
+  },
+  {
+    "revision": "c641b27501ee08322683683a9b843fdb",
+    "url": "/portfolio/static/media/shimmer.c641b275.jpg"
+  }
+]);
