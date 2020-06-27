f85e20390761573a063df2a373ddc34c22379dcd
daveman235 <awad.david@gmail.com>
2020-06-27 11:43:10 -0400

    trying something to fix scrolling

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import theme from "../src/MuiTheme";
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </MuiThemeProvider>,
   document.getElementById('root')
 );
